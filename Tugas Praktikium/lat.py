5f9dd3cd02dc8a82d0e81236d651e2a7356d27aa
akiljhe <aqiljhe@gmail>
2025-09-12 09:57:12 +0800

    akil gatrngf

---
+++ Tugas Praktikium/lat.py
@@ -0,0 +1,15 @@
+jam_pertama = 5000
+jam_kedua = 3000
+biaya = (int (input("masukkan berapa lama :")))
+biaya_akhir = jam_pertama + (jam_kedua * biaya) - jam_kedua
+print(biaya_akhir)
+
+# tiket1 = (int(input("Jumlah Tiket Dewasa : ")))
+# tiket2 = (int(input("Jumlah Tiket anak-anak : ")))
+# tiketdewasa = 50000
+# tiketanak = 30000
+# x = tiketdewasa * tiket1
+# y = tiketanak * tiket2
+# total = ((x+y) * 0.05) + (x+y)
+# print ("Total : ", total)
+# print ("Total biaya Rp.",biaya)
